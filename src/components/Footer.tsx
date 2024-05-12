01d45dbda3e3f56abd596f9bc96cbe2aa0e02f05
Rahul Kumar <rahulkhg980@gmail.com>
2024-05-12 12:10:58 +0530

    Corrected address icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 lg:grid-cols-3 
         md:grid-cols-2 w-[80%] mx-auto gap-[3rem] ">
                 <div className="flex items-center space-x-6">
-                    <div className="md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center
+                    <div className="md:w-[9rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center
                 rounded-full bg-[#55e6a5] ">
                         <MapIcon className="md:w-[4rem] w-[5rem] text-black" />
                     </div>
